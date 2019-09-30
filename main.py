bd844749851761ad56cd0197fc14cd4e0ba5e885
StanislavMyakishev <masiff-s@live.ru>
2019-10-01 00:04:48 +0300

    minor fixes - now stable

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from classes.calculator import Calculator
 from classes.reader import Reader
 from classes.writer import Writer
 from classes.fileReader import fileReader
+from classes.fileWriter import filewriter
 
 fr = fileReader()
 fr.readFilePath()
