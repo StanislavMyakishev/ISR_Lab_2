02c26cee3b7d05a25758860409ed7cd5f36af34c
StanislavMyakishev <masiff-s@live.ru>
2019-09-30 23:24:37 +0300

    fileReader class + test file

---
+++ classes/fileReader.py
@@ -0,0 +1,44 @@
+from classes.calculator import Calculator
+from classes.reader import Reader
+from classes.writer import Writer
+
+class fileReader:
+    InitMessage = '*** Write file name'
+    FilePath = ''
+    isRPN = False
+    isFIB = False
+    isFAC = False
+    CalcExpression = ''
+    def readFilePath(self):
+        print(self.InitMessage)
+        print('>>> ', end='')
+        self.FilePath = input()
+    def doMath(self):
+        if (self.FilePath == ''):
+            self.FilePath = 'input.txt'
+        f = open(self.FilePath)
+        OPERATORS = ['+', '-', '*', '/', ' ']
+        for line in f:
+            self.CalcExpression = line.replace('\n', '')
+            if (self.CalcExpression[0] == '$'):
+                self.isRPN = True
+                self.CalcExpression = self.CalcExpression.replace('$', '')
+            elif (self.CalcExpression[0] == 'F'):
+                self.isFIB = True
+                self.CalcExpression = self.CalcExpression.replace('F', '')
+                self.CalcExpression = self.CalcExpression.replace(' ', '')
+            elif (self.CalcExpression[0] == '!'):
+                self.isFAC = True
+                self.CalcExpression = self.CalcExpression.replace('!', '')
+                self.CalcExpression = self.CalcExpression.replace(' ', '')
+            else:
+                if (len(self.CalcExpression.split(' ')) != 3):
+                    print('The input expression is incorrect')
+                for token in self.CalcExpression:
+                    if (not token.isdecimal() and not token in OPERATORS):
+                        print('The input expression is incorrect')
+            c = Calculator(self.isRPN, self.isFIB, self.isFAC, self.CalcExpression)
+            calculus = c.calc()
+            err = c.getErr()
+            w = Writer(calculus, err)
+            w.printResult()
