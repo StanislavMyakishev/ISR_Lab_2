a798756813722bf0de8dc2e6700558a2d6c3711c
StanislavMyakishev <masiff-s@live.ru>
2019-09-30 18:59:42 +0300

    Fibonacci sequence option added

--- classes/reader.py
+++ classes/reader.py
@@ -3,8 +3,9 @@
 # readCalcExpression - reads expr from input and checks if it RPN
 # isRPN - returns RPN param
 class Reader:
-    InitMessage = '*** Write the calc expression\n*** Put "$" symbol first to calculate the expression in Reverse Polish notation\n*** If the expression is not in RPN - write the expression according to the template: "(arg1) (operation) (arg2)"\n*** Suported operations list: "+", "-", "*", "/", ' 
+    InitMessage = '*** Write the calc expression\n*** Put "$" symbol first to calculate the expression in Reverse Polish notation\n*** If the expression is not in RPN - write the expression according to the template: "(arg1) (operation) (arg2)"\n*** Suported operations list: "+", "-", "*", "/" \n*** Put "F" before a number to calc n-th Fibonacci sequence number' 
     CalcExpression = ''
+    FIB = False
     RPN = False
     def pringInitMessage(self):
         print(self.InitMessage)
@@ -15,6 +16,10 @@ class Reader:
         if (self.CalcExpression[0] == '$'):
             self.RPN = True
             self.CalcExpression = self.CalcExpression.replace('$', '')
+        elif (self.CalcExpression[0] == 'F'):
+            self.FIB = True
+            self.CalcExpression = self.CalcExpression.replace('F', '')
+            self.CalcExpression = self.CalcExpression.replace(' ', '')
         else: 
             if (len(self.CalcExpression.split(' ')) != 3):
                 return 'The input expression is incorrect'
@@ -24,3 +29,5 @@ class Reader:
         return  self.CalcExpression
     def isRPN(self):
         return self.RPN
+    def isFIB(self): 
+        return self.FIB
